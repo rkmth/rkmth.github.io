d306ccd8ed4852b87f32d76de3ca6d62f3e03a84
raghu <raghavendra.kamath@alexanderthamm.com>
2022-01-31 12:00:28 +0100

    Added multiple repos

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,26 @@
 module.exports = {
-    siteMetadata: {
-      title: `testy`,
-        siteUrl: `https://www.yourdomain.tld`,
+  plugins: [
+    // You can have multiple instances of this plugin to read source files from
+    // different repositories or locations within a repository.
+    {
+      resolve: `gatsby-source-git`,
+      options: {
+        name: `repo-one`,
+        remote: `https://github.com/rkmth/docmodule1.git`,
+        branch: `master`,
+        // Only import the docs folder from a codebase.
+        patterns: `docs/**`
+      }
     },
-    plugins: [
-
-    ]
-}
+    {
+      resolve: `gatsby-source-git`,
+      options: {
+        name: `repo-one`,
+        remote: `https://github.com/rkmth/docmodule2.git`,
+        branch: `master`,
+        // Only import the docs folder from a codebase.
+        patterns: `docs/**`
+      }
+    }
+  ]
+};
